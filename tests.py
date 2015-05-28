d5d29715488319fca2ed8219d5412ca7abf67c20
Chronium <onlivechronium@gmail.com>
2015-05-28 09:04:47 +0300

    Added some more unit tests.

--- tests.py
+++ tests.py
@@ -10,15 +10,14 @@ def parse_line_visit(line):
     node, indents = parser.parse_immediate(lexer)
     return node.visit()
 
-def parse_block(block):
-    lexer = Lexer(block, matchers)
-    parser = Parser()
-    return parser.parse(lexer)
-
 def test_simple_math():
     assert parse_line_visit('1 + 2\n') == 3
 def test_order_operations():
     assert parse_line_visit('1 + 2 * 3\n') == 7
+def test_variable_undefined():
+    assert parse_line_visit('1 * 2 - x * 3\n') == 0
 def test_variable():
     parse_line_visit('x = 1 + 2')
     assert parse_line_visit('1 * 2 - x * 3\n') == -7
+def test_function():
+    assert parse_line_visit('7 + ret5()') == 12
